4b7ad46f1130e9265ae9e809d05ed499e00f253d
Arruda <arruda@uniriotec.com>
2011-06-13 23:52:00 -0300

    first commit

---
+++ src/tcp/TCPServer.java
@@ -0,0 +1,36 @@
+package tcp;
+import java.io.*; 
+import java.net.*; 
+
+class TCPServer { 
+
+  public static void main(String argv[]) throws Exception 
+    { 
+      String clientSentence; 
+      String capitalizedSentence; 
+
+      ServerSocket welcomeSocket = new ServerSocket(6789); 
+
+ 	 System.out.println("rodando");
+      while(true) { 
+    	  
+            Socket connectionSocket = welcomeSocket.accept(); 
+
+           BufferedReader inFromClient = 
+              new BufferedReader(new
+              InputStreamReader(connectionSocket.getInputStream())); 
+
+
+
+           DataOutputStream  outToClient = 
+             new DataOutputStream(connectionSocket.getOutputStream()); 
+
+           clientSentence = inFromClient.readLine(); 
+
+           capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
+
+           outToClient.writeBytes(capitalizedSentence); 
+        } 
+    } 
+} 
+ 
